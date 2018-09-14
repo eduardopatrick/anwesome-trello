24ff2e55e96523def783c982d5436824a7ec2c25
Eduardo Patrick <eduardo.patrick.98@gmail.com>
2018-09-14 18:48:15 -0300

    First Commit - starting home page

---
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -0,0 +1,18 @@
+import { NgModule, Component } from '@angular/core';
+
+
+
+@Component({
+    selector: 'app-toolbar',
+    templateUrl: './toolbar.component.html',
+    styleUrls: ['./toolbar.component.css']
+  })
+
+@NgModule({
+
+})
+
+
+export class ToolbarComponent  {
+
+ }
